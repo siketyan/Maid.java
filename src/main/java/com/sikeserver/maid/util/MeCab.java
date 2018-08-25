5018bf0e7e974dfe2242526105c440dfc565ef49
Siketyan <siketyan@sikeserver.com>
2018-08-25 14:53:04 +0900

    Add MeCab analyzing

---
+++ src/main/java/com/sikeserver/maid/util/MeCab.java
@@ -0,0 +1,32 @@
+package com.sikeserver.maid.util;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MeCab {
+    public static List<String> analyze(String text) throws IOException {
+        var process = Runtime.getRuntime().exec("mecab");
+
+        try (var stream = process.getOutputStream();
+             var writer = new BufferedWriter(new OutputStreamWriter(stream))) {
+            writer.write(text);
+            writer.flush();
+        }
+
+        var result = new ArrayList<String>();
+        try (var stream = process.getInputStream();
+             var reader = new BufferedReader(new InputStreamReader(stream))) {
+            while (true) {
+                var line = reader.readLine();
+                if (line == null) {
+                    break;
+                }
+
+                result.add(line);
+            }
+        }
+
+        return result;
+    }
+}
