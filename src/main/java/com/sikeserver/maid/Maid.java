bc231677471d6a83ad2a62b6ae07d6c128844ffc
Siketyan <siketyan@sikeserver.com>
2018-08-25 15:29:12 +0900

    Add SQLManager

--- src/main/java/com/sikeserver/maid/Maid.java
+++ src/main/java/com/sikeserver/maid/Maid.java
@@ -3,6 +3,7 @@ package com.sikeserver.maid;
 import com.sikeserver.maid.server.SocketServlet;
 import com.sikeserver.maid.util.Logger;
 import com.sikeserver.maid.util.NoLogging;
+import com.sikeserver.maid.util.SQLManager;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerCollection;
 import org.eclipse.jetty.servlet.ServletContextHandler;
@@ -11,12 +12,14 @@ import org.eclipse.jetty.util.log.Log;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class Maid {
     private static final String CONFIG_FILE = "maid.properties";
 
     private static Properties conf;
+    private static SQLManager sql;
 
     public static void main(String[] args) {
         conf = new Properties();
@@ -28,6 +31,25 @@ public class Maid {
             Logger.info("Configuration file does not exists, applying default");
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        try {
+            sql = new SQLManager(
+                conf.getProperty("SQL.Type", "mysql"),
+                conf.getProperty("SQL.Host", "localhost"),
+                Integer.parseUnsignedInt(conf.getProperty("SQL.Port", "3306")),
+                conf.getProperty("SQL.Database", "maid"),
+                conf.getProperty("SQL.User", "maid"),
+                conf.getProperty("SQL.Password", ""),
+                Integer.parseUnsignedInt(conf.getProperty("SQL.Timeout", "1"))
+            );
+
+            Logger.success("Connected to SQL server.");
+        } catch (SQLException e) {
+            Logger.error("Failed to connect to SQL server");
+            e.printStackTrace();
+
+            return;
         }
 
         var server = new Server(Integer.parseUnsignedInt(conf.getProperty("Server.Port", "8080")));
@@ -52,5 +74,9 @@ public class Maid {
 
     public static Properties getConfig() {
         return conf;
+    }
+
+    public static SQLManager getSQL() {
+        return sql;
     }
 }
